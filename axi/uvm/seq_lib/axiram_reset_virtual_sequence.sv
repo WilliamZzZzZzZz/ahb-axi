ca42abab045b887abea208ee49a09f1656fe778d
WilliamZzz <zchenkb@connect.ust.hk>
2026-03-17 20:41:05 +0800

    finish read-mid-reset-test

--- axi/uvm/seq_lib/axiram_reset_virtual_sequence.sv
+++ axi/uvm/seq_lib/axiram_reset_virtual_sequence.sv
@@ -10,6 +10,7 @@ class axiram_reset_virtual_sequence extends axiram_base_virtual_sequence;
 
     virtual task body();
         write_mid_reset_test();
+        read_mid_reset_test();
     endtask
 
     virtual task write_mid_reset_test();
@@ -17,24 +18,24 @@ class axiram_reset_virtual_sequence extends axiram_base_virtual_sequence;
         every_beat_data = new[8];
         foreach (every_beat_data[i]) every_beat_data[i] = 32'hf0f0_0000 + 1;
         
-        `uvm_info(get_type_name(), "--- test_write_mid_reset START ---", UVM_LOW)
+        `uvm_info(get_type_name(), "--- write_mid_reset_test START ---", UVM_LOW)
         fork
-            begin
+            begin:WRITE_ACTION
                 single_write = axiram_single_write_sequence::type_id::create("single_write");
-                single_write.addr = 16'h0300;
-                single_write.data = every_beat_data[0];
-                single_write.every_beat_data = every_beat_data;
-                single_write.burst_len = BURST_LEN_8BEATS;
-                single_write.burst_size = BURST_SIZE_4BYTES;
-                single_write.burst_type = INCR;
+                single_write.addr               = 16'h0300;
+                single_write.data               = every_beat_data[0];
+                single_write.every_beat_data    = every_beat_data;
+                single_write.burst_len          = BURST_LEN_8BEATS;
+                single_write.burst_size         = BURST_SIZE_4BYTES;
+                single_write.burst_type         = INCR;
                 single_write.start(p_sequencer);
             end
-            begin
+            begin:RESET_CONTROL
                 //wait AW handshake
                 @(posedge vif.aclk iff(vif.awvalid && vif.awready));
-                //let 3 beats transfer complete
+                //let W 3 beats transfer complete
                 repeat(3) @(posedge vif.aclk iff(vif.wvalid && vif.wready));
-                //assert reset
+                //force reset
                 vif.assert_reset();
                 //wait 2 posedge for DUT registers to update(doubt!)
                 repeat(2) @(posedge vif.aclk);
@@ -46,10 +47,42 @@ class axiram_reset_virtual_sequence extends axiram_base_virtual_sequence;
                 wait_cycles(5);
             end
         join_any
-        `uvm_info(get_type_name(), "--- test_write_mid_reset END ---", UVM_LOW)
+        `uvm_info(get_type_name(), "--- write_mid_reset_test END ---", UVM_LOW)
 
     endtask
 
+    virtual task read_mid_reset_test();
+        `uvm_info(get_type_name(), "--- read_mid_reset_test START ---", UVM_LOW)
+        fork
+            begin:READ_ACTION
+                single_read = axiram_single_read_sequence::type_id::create("single_read");
+                single_read.addr        = 16'h0300;
+                single_read.burst_len   = BURST_LEN_8BEATS;
+                single_read.burst_size  = BURST_SIZE_4BYTES;
+                single_read.burst_type  = INCR;
+                single_read.start(p_sequencer);
+            end
+            begin:RESET_CONTROL
+                //wait AR handshake
+                @(posedge vif.aclk iff(vif.arvalid && vif.arready));
+                //let R 3 transfer complete
+                repeat(3) @(posedge vif.aclk iff(vif.rvalid && vif.rready));
+                //force reset
+                vif.assert_reset();
+                //wait 2 cycles
+                repeat(2) @(posedge vif.aclk);
+                //check DUT signals
+                check_read_reset_signals();
+                //hold reset for some cycles, then release
+                repeat(3) @(posedge vif.aclk);
+                vif.deassert_reset();
+                wait_cycles(5);
+            end
+        join_any
+        `uvm_info(get_type_name(), "--- read_mid_reset_test END ---", UVM_LOW)
+    endtask
+
+    //DUT as a slave, only check DUT's output signals
     local task check_write_reset_signals();
         if(vif.awready !== 1'b0)
             `uvm_error(get_type_name(), $sformatf("RESET FAIL: awready=%b, exp=0", vif.awready))
@@ -58,6 +91,14 @@ class axiram_reset_virtual_sequence extends axiram_base_virtual_sequence;
         if(vif.bvalid !== 1'b0)
             `uvm_error(get_type_name(), $sformatf("RESET FAIL: bvalid=%b, exp=0", vif.bvalid))
         `uvm_info(get_type_name(), "Write reset signals checked OK", UVM_MEDIUM)
+    endtask
+
+    //DUT as a slave, only check DUT's output signals
+    local task check_read_reset_signals();
+        if(vif.arready !== 1'b0)
+            `uvm_error(get_type_name(), $sformatf("RESET FAIL: arready=%b, exp=0", vif.arready))
+        if(vif.rvalid !== 1'b0)
+            `uvm_error(get_type_name(), $sformatf("RESET FAIL: rvalid=%b, exp=0", vif.rvalid))
     endtask
 
     local task do_single_write(bit [15:0] addr, bit [31:0] data);
